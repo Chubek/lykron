e402cbcd75188171f6841c18003b8746bc9e8906
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-07-29 21:48:10 +0330

    Added Type Structures: For tabs, environ, jobs, etc

--- lykron.h
+++ lykron.h
@@ -1,14 +1,86 @@
 #ifndef LYKRON_H
 #define LYKRON_H
 
+#define _POSIX_SOURCE
+#define _POSIX_C_SOURCE
+#include <limits.h>
+#include <pwd.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <wordexp.h>
+
+#define NUM_Mins 60
+#define NUM_Hours 24
+#define NUM_DoM 31
+#define NUM_Month 12
+#define NUM_DoW 7
+
+typedef struct TabParser
+{
+
+} TabParser;
+
+typedef struct TabLinter
+{
+
+} TabLinter;
+
+typedef struct Timeset
+{
+  bool mins[NUM_Mins];
+  bool hours[NUM_Hours];
+  bool dom[NUM_DoM];
+  bool month[NUM_Month];
+  bool dow[NUM_DoW];
+} Timeset;
+
+typedef struct CronJob
+{
+  Timeset timeset;
+  const uint8_t *command;
+  size_t command_len;
+  const char user[LOGIN_NAME_MAX + 1];
+  uid_t uid;
+  gid_t gid;
+  pid_t pid;
+} CronJob;
+
+typedef struct CronTab
+{
+  const char on_disk[PATH_MAX + 1];
+  time_t mtime;
+  TabParser *parser;
+  TabLinter *linter;
+} CronTab;
+
+typedef struct Environ
+{
+  struct Symbol
+  {
+    const uint8_t *name;
+    const uint8_t *value;
+  } *symbols;
+  size_t num_symbols;
+
+  char **original_environ;
+  const char shell[PATH_MAX + 1];
+
+  const char (*tab_dirs)[PATH_MAX + 1];
+  size_t num_tab_dirs;
+
+  CronTab *tabs;
+  size_t num_tabs;
+
+  CronJob *jobs;
+  size_t num_jobs;
+} Environ;
 
 typedef struct EventHandler
 {
