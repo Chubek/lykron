c69000b39c10a126dd4a8106260e2ee1eaa9aec2
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-07-30 00:55:16 +0330

    Added; Cronjob constructors/linkers

--- job.c
+++ job.c
@@ -1,3 +1,6 @@
+#include <pwd.h>
+#include <sys/types.h>
+
 #include "lykron.h"
 
 time_t
@@ -26,6 +29,52 @@ timesetComputeNextOccurance (Timeset *tset, time_t now)
     }
 
   return (time_t)TIME_UNSPEC;
+}
+
+CronJob *
+cronJobNew (Timeset *ts, const uint8_t *command, size_t command_len,
+            const char *user)
+{
+  CronJob *cj = memAllocSafe (sizeof (CronJob));
+  cj->command = strndup (command, command_len);
+  cj->command_len = command_len;
+  cj->next = NULL;
+
+  memmove (&cj->timeset, ts, sizeof (Timeset));
+
+  if (user == NULL)
+    strncat (&cj->user[0], "root", 4);
+  else
+    strncat (&cj->user[0], user, LOG_NAME_MAX);
+
+  struct passwd *pwd = getpwnam (&cj->user[0]);
+  cj->uid = pwd->pw_uid;
+  cj->gid = pwd->pw_gid;
+
+  return cj;
+}
+
+void
+cronJobDelete (CronJob *cj)
+{
+  if (cj == NULL)
+    return;
+
+  CronJob *next = cj->next;
+
+  memDeallocSafe (cj->command);
+  memDeallocSafe (cj);
+
+  cronJobDelete (next);
+}
+
+void
+cronJobListLink (CronJob *hcj, CronJob *ncj)
+{
+  CronJob *tcj = NULL;
+  for (tcj = hcj; tcj->next; tcj = tcj->next)
+    ;
+  tcj->next = ncj;
 }
 
 void
