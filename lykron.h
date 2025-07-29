537f33c6070a495a4da3a5ffa547922a7fa37662
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-07-30 00:16:01 +0330

    CorrCommit: corresponding to the last refactor commit

--- lykron.h
+++ lykron.h
@@ -22,16 +22,6 @@
 #define NUM_Month 12
 #define NUM_DoW 7
 
-typedef struct TabParser
-{
-
-} TabParser;
-
-typedef struct TabLinter
-{
-
-} TabLinter;
-
 typedef struct Timeset
 {
   bool mins[NUM_Mins];
@@ -52,46 +42,10 @@ typedef struct CronJob
   pid_t pid;
 } CronJob;
 
-typedef struct CronTab
-{
-  const char on_disk[PATH_MAX + 1];
-  time_t mtime;
-  TabParser *parser;
-  TabLinter *linter;
-} CronTab;
-
-typedef struct Environ
-{
-  struct Symbol
-  {
-    const uint8_t *name;
-    const uint8_t *value;
-  } *symbols;
-  size_t num_symbols;
-
-  char **original_environ;
-  const char shell[PATH_MAX + 1];
-
-  const char (*tab_dirs)[PATH_MAX + 1];
-  size_t num_tab_dirs;
-
-  CronTab *tabs;
-  size_t num_tabs;
-
-  CronJob *jobs;
-  size_t num_jobs;
-} Environ;
-
-typedef struct EventHandler
-{
-  const uint8_t *command;
-  size_t command_len;
-} EventHandler;
-
 typedef struct EventNotice
 {
   time_t time;
-  EventHandler handler;
+  CronJob *job;
   int bucket_idx;
   struct EventNotice *prev, *next;
 } EventNotice;
