c9192eb71a9c2e46e46c87722af6823b416ff835
Chubak Bidpaa <chubakbidpaa@riseup.net>
2025-07-30 07:44:52 +0330

    Init, Added: added basic constructors/destructors, plus the `isModified` function

---
+++ tab.c
@@ -0,0 +1,63 @@
+#define POSIX_SOURCE
+#define POSIX_C_SOURCE
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "lykron.h"
+
+CronTab *
+crontabNew (const char *path, const char *user, bool is_main)
+{
+  CronTab *ct = memAllocSafe (sizeof (CronTab));
+  ct->path = strndup (&ct->path[0], path, PATH_MAX);
+  ct->user = strndup (&ct->user[0], user, LOGIN_NAME_MAX);
+  ct->is_main = is_main;
+  ct->next = NULL;
+
+  struct stat st = { 0 };
+  if (stat (&ct->path[0], &st) < 0)
+    errorOut ("stat");
+
+  ct->mtime = st.st_mtim.tv_sec;
+}
+
+void
+crontabListDelete (CronTab *ct)
+{
+  if (ct == NULL)
+    return;
+
+  CronTab *next = ct->next;
+  memDeallocSafe (ct);
+
+  crontabListDelete (next);
+}
+
+void
+crontabListLink (CronTab *hct, CronTab *nct)
+{
+  CronTab *tct = NULL;
+  for (tct = hct; tct->next; tct = tct->next)
+    ;
+  tct->next = nct;
+}
+
+bool
+crontabIsModified (CronTab *ct)
+{
+  struct stat st;
+  if (stat (&ct->path[0], &st) < 0)
+    errorOut ("stat");
+
+  if (ct->mtime < st.st_mtim.tv_sec)
+    {
+      ct - mtime = st.st_mtim.tv_sec;
+      return true;
+    }
+
+  return false;
+}
